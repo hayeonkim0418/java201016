fa8e504bb9b3f451e95452d5c0133268dd30659d
hayeonkim0418 <gkdus0418@hanmail.net>
2020-10-28 12:49:55 +0900

    singup

---
+++ java201028/SingUp.java
@@ -0,0 +1,26 @@
+package java201028;
+
+public class SingUp {
+	String id;
+	int pw;
+	String nickname;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getPw() {
+		return pw;
+	}
+	public void setPw(int pw) {
+		this.pw = pw;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+}
