28170f7bf52fbeb81de6843370578c9277393787
hayeonkim0418 <72956163+hayeonkim0418@users.noreply.github.com>
2020-10-30 01:11:06 +0900

    sign up/in

---
+++ board_03/ArticleDao.java
@@ -0,0 +1,76 @@
+package board_03;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ArticleDao {
+	private ArrayList<Article> articles;
+	private int no = 4;
+
+	public ArticleDao() {
+		articles = new ArrayList<>();
+
+		Article a1 = new Article(1, "안녕1", "내용1", "익명", getCurrentDate());
+		Article a2 = new Article(2, "반가워2", "내용2", "익명", getCurrentDate());
+		Article a3 = new Article(3, "안녕하세요3", "내용3", "익명", getCurrentDate());
+
+		articles.add(a1);
+		articles.add(a2);
+		articles.add(a3);
+	}
+
+	public Article getArticleById(int targetId) {
+		for (int i = 0; i < articles.size(); i++) {
+			int id = articles.get(i).getId();
+			if (id == targetId) {
+				return articles.get(i);
+			}
+		}
+
+		return null;
+	}
+
+	private static String getCurrentDate() {
+		SimpleDateFormat format1 = new SimpleDateFormat("yyyy.MM.dd");
+		Date time = new Date();
+		String time1 = format1.format(time);
+
+		return time1;
+	}
+
+	public ArrayList<Article> getSearchByFlag(int flag, String keyword) {
+
+		ArrayList<Article> searchArticle = new ArrayList<>();
+
+		for (int i = 0; i < articles.size(); i++) {
+			Article article = articles.get(i);
+			String str = article.getPropertiesByFlag(flag);
+
+			if (str.contains(keyword)) {
+				searchArticle.add(article);
+			}
+		}
+		return searchArticle;
+	}
+
+//    public ArrayList<Article> getReadByFlag(int flag){
+//    	ArrayList<Article> ReadArticle = new ArrayList<>();
+//    	
+//		return ReadArticle;
+//    }
+
+	public void removeArticle(Article a) {
+		articles.remove(a);
+	}
+
+	public void insertArticle(Article a) {
+		a.setId(no);
+		no++;
+		articles.add(a);
+	}
+
+	public ArrayList<Article> getArticles() {
+		return articles;
+	}
+}
---
+++ board_03/MemberDao.java
@@ -0,0 +1,54 @@
+package board_03;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MemberDao {
+	private ArrayList<Member> members;
+	private int no = 1;
+	
+	public MemberDao() {
+		members = new ArrayList<>();
+
+//		Article a1 = new Article(1, "안녕1", "내용1", "익명", getCurrentDate());
+//		Article a2 = new Article(2, "반가워2", "내용2", "익명", getCurrentDate());
+//		Article a3 = new Article(3, "안녕하세요3", "내용3", "익명", getCurrentDate());
+//
+//		articles.add(a1);
+//		articles.add(a2);
+//		articles.add(a3);
+	}
+
+	public Member getArticleById(int targetId) {
+		for (int i = 0; i < members.size(); i++) {
+			int id = members.get(i).getId();
+			if (id == targetId) {
+				return members.get(i);
+			}
+		}
+
+		return null;
+	}
+
+	private static String getCurrentDate() {
+		SimpleDateFormat format1 = new SimpleDateFormat("yyyy.MM.dd");
+		Date time = new Date();
+		String time1 = format1.format(time);
+
+		return time1;
+	}
+	
+
+	public void insertMember(Member m) {
+		m.setId(no);
+		no++;
+		m.setRegDate(getCurrentDate());
+		members.add(m);
+	}
+	
+	public ArrayList<Member> getMembers() {
+		return members;
+	}
+
+}
